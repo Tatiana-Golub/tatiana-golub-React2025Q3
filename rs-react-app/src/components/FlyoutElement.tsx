834b23b072c9c20183ca21b5dc78253732c12563
Tatiana Golub <tatiana.k1989@gmail.com>
2025-08-02 20:04:47 +0200

    feat: add displaying number of selected items and unselect all feature

--- rs-react-app/src/components/FlyoutElement.tsx
+++ rs-react-app/src/components/FlyoutElement.tsx
@@ -1,9 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux';
+import type { RootState } from '../redux/store';
+import { unselectAll } from '../redux/cardSlice';
+
 function FlyoutElement() {
+  const dispatch = useDispatch();
+  const { selectedItemsIds } = useSelector(
+    (state: RootState) => state.selectedItems
+  );
+
+  function onUnselectClick() {
+    dispatch(unselectAll());
+  }
+
+  const selectedItemsCount = selectedItemsIds.length;
+
+  if (selectedItemsCount === 0) return null;
+
   return (
     <div className="flyout-element">
-      <span className="selected-items-info">Item(s) selected: {1}</span>
+      <span className="selected-items-info">
+        Item(s) selected: {selectedItemsCount}
+      </span>
       <div className="flyout-buttons">
-        <button className="flyout-button" onClick={() => console.debug(true)}>
+        <button className="flyout-button" onClick={onUnselectClick}>
           Unselect all
         </button>
         <button className="flyout-button" onClick={() => console.debug(true)}>
