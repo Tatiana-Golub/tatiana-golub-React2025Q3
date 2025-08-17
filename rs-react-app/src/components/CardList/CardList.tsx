b6d56831b70796cd8f4a0a69970b6f1aa840664e
Tatiana Golub <tatiana.k1989@gmail.com>
2025-08-17 23:38:33 +0200

    fix: CardList hydration fail

--- rs-react-app/src/components/CardList/CardList.tsx
+++ rs-react-app/src/components/CardList/CardList.tsx
@@ -4,15 +4,25 @@ import type { CardListProps } from '../../types';
 import { useTranslations } from 'next-intl';
 import { useParams } from 'next/navigation';
 import DetailsCard from '../DetailsCard';
+import { useEffect, useState } from 'react';
 
 function CardList({ data }: CardListProps) {
+  const [hydrated, setHydrated] = useState(false);
   const t = useTranslations('CardList');
   const { locale, pageNumber } = useParams<{
     locale: string;
     pageNumber: string[];
   }>();
 
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+
   const [page, id] = pageNumber;
+
+  if (!hydrated) {
+    return <div className={styles.mainContainer}></div>;
+  }
 
   if (data.length === 0)
     return <p className={styles.emptySearchMessage}>{t('error')}</p>;
